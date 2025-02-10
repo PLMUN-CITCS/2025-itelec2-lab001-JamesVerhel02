050d5a2312d42138f53af9bb5382511999c4fc99
James Verhel <quizonjamesverhel_bsit@plmun.edu.ph>
2025-02-11 00:14:27 +0800

    My first Python program

---
+++ hello.py
@@ -0,0 +1,4 @@
+print("Hello World!")
+print("Name: James Verhel Dela Soledad Quizon")
+print("Course: Bachelor of Science in Information Technology ")
+print("Interests in programming: I'm interested to learn everything.")
